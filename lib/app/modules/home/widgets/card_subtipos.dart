d0d084f5ec028326c5df8fe4c29582b4326dcc39
Mario Moraes <moraes_mario@hotmail.com>
2023-10-25 21:38:33 -0300

    Changes

--- lib/app/modules/home/widgets/card_subtipos.dart
+++ lib/app/modules/home/widgets/card_subtipos.dart
@@ -17,32 +17,22 @@ class CardSubTipos extends StatelessWidget {
       onTap: () => Modular.to.pushNamed('/subtipos',
           arguments: {'id': id, 'descricao': descricao, 'icon': icon}),
       child: Container(
-        height: 50,
-        width: 100,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(20),
+        height: 40,
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border.all(color: Colors.grey.withOpacity(0.5)),
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 80,
-              child: Center(
-                child: Text(
-                  descricao,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        child: Text(
+          descricao,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+            color: Colors.black,
+            overflow: TextOverflow.clip,
+          ),
         ),
       )
           .animate()
@@ -52,11 +42,7 @@ class CardSubTipos extends StatelessWidget {
             duration: 500.ms,
             curve: Curves.decelerate,
           )
-          .fadeIn()
-          .blurXY(
-            begin: 10,
-            end: 0,
-          ),
+          .fadeIn(),
     );
   }
 }
